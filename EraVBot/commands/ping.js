04dbf66ba5b5c5cdacdf12f23aa0743ca64064a9
ZedLaMoula <85707153+ZedTahLaNight@users.noreply.github.com>
2021-06-30 21:45:40 +0200

    Delete EraVBot directory

--- EraVBot/commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'ping',
-  description: 'Test Command.',
-  execute(message) {
-    message.channel.send('Pong.');
-  }
-};
--- EraVBot/commands/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'server',
-  description: 'Server informations.',
-  execute(message) {
-    message.channel.send(`Nom du serveur : ${message.guild.name}\n Nombre d'utilisateurs : ${message.guild.memberCount}`);
-  }
-};
